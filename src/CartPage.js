468459db03c523dc0ef3b92505c44c2be05446ba
JerYOMat <jeremy.emata@gmail.com>
2019-03-16 02:00:23 -0400

    basic CartPage component, missing addToCart prop on Item

---
+++ src/CartPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Item from './Item';
+import './CartPage.css';
+
+function CartPage({ items }) {
+  return (
+    <ul className='CartPage-items'>
+      {items.map(item =>
+        <li key={item.id} className='CartPage-item'>
+          <Item item={item} />
+        </li>
+      )}
+    </ul>
+  );
+}
+CartPage.propTypes = {
+  items: PropTypes.array.isRequired
+};
+
+export default CartPage;
