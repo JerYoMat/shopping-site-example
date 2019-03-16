66b27655f2e2faac0e62598b24da72cddcc53be1
JerYOMat <jeremy.emata@gmail.com>
2019-03-16 00:43:36 -0400

    added click event listener to Nav, stored stated in App and included basic styling.

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import Nav from './Nav';
 import './App.css';
 
 class App extends Component {
+  state = {
+    activeTab: 0
+  };
+  handleTabChange = (index) => { 
+    this.setState({
+      activeTab: index
+    });
+  }
   renderContent() {
-    return (
-      <span>Empty</span>
-    );
+    switch(this.state.activeTab) {
+      default:
+      case 0: return <span>Items Placeholder</span>;
+      case 1: return <span>Cart Placeholder</span>;
+    }
   }
   render() {
+    let {activeTab} = this.state;
     return (
       <div className="App">
-        <Nav />
+        <Nav activeTab={activeTab} onTabChange={this.handleTabChange} />
         <main className="App-content">
           {this.renderContent()}
         </main>
