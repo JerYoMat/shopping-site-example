dd66fa0fe79d1fc11d3e591071aef3272628c01f
JerYOMat <jeremy.emata@gmail.com>
2019-03-15 23:23:35 -0400

    added App and Nav skeleton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Nav from './Nav';
 import './App.css';
 
 class App extends Component {
+  renderContent() {
+    return (
+      <span>Empty</span>
+    );
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Nav />
+        <main className="App-content">
+          {this.renderContent()}
+        </main>
       </div>
     );
   }
